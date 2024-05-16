b63112f8303e34b2cff5424b42acc579a0406e41
RAF123E <raniaf61q@gmail.com>
2024-05-16 19:46:34 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,301 @@
+/*
+// var x = window.prompt('Enter Your Number :');                       
+//  console.log(x);*/
+
+// ---------------------------------------------------------------------------------------
+// var x = window.prompt('Enter Your Number : ');
+//  if ( x % 3 ==0 && x % 4 == 0){
+//     console.log('Yes');
+//  }else{                                                             tow
+//     console.log('No');
+//  }
+// ----------------------------------------------------------------------------------------
+
+// var x=+window.prompt('enter :');
+// var y=+window.prompt('enter :');    
+// if(x > y)                                                            three
+// {               
+// console.log(x);
+// }else{
+//     console.log(y);
+// }
+// ----------------------------------------------------------------------------------------
+// var x
+// x=window.prompt('Enter Your Number Please')
+// if(x<0){                                                                    four
+//     console.log('Negative');
+// }else{
+//     console.log('posetive');
+// }
+// -----------------------------------------------------------------------------------------
+
+// var x
+// var y
+// var z
+// x=window.prompt('enter your number please');
+// y=window.prompt('enter your number please');
+// z=window.prompt('enter your number please');
+// if (x>y && x>z && y>z) {
+//     console.log('max element =', x);
+//     console.log('min element =',z);
+// }else if(z>x && z>y && y>x ){                                             five
+//     console.log('max element =',z);
+//     console.log('min element =',x);
+// }else if (z>x && z>y && x>y) {
+//     console.log('max element =',z);                                                    
+//     console.log('min element =',y);
+// }else if (x<y && x<z && y<z) {
+//     console.log('max element =',z);
+//     console.log('min element =',x);
+// }else if (x<y && x>z && y>z) {
+//     console.log('max element =',y);
+//     console.log('min element =',z);
+//  }else if (x<y && x<z && y>z) {
+//     console.log('max element =',y);
+//     console.log('min element = ',x);  
+// }else if (x>y && x>z && y<z) {
+//     console.log('max element =',x);
+//     console.log('min element = ',y);
+// }else{
+//     console.log('false');
+// }
+//  ------------------------------------------------------------------------------------------
+// var x=window.prompt('Enter Your Number :')
+// if (x%2==0) {                                                              sex
+//     console.log('even');
+// }else{
+//     console.log('odd');
+// }
+// ---------------------------------------------------------------------------------------------
+
+// var x=window.prompt('Enter Your character :') ;
+// if (x=='a' || x=='o' || x=='i' || x=='e' || x=='u') {                      eight
+//     console.log(' vowel ');
+// }else{
+//     console.log(' Consonant ');
+// }
+// ---------------------------------------------------------------------------------------------
+// var x=window.prompt('Enter Your Number :')
+// for (var i = 1; i <= x ; i++) {                                              nine
+//     console.log(i);                                                                            
+// }
+// ---------------------------------------------------------------------------------------------
+// var x=window.prompt('Enter Your Number :')
+
+// for (var i = 1; i<=12; i++) {                                                  ten
+//     console.log(i*x);                                                                            
+// }
+// ---------------------------------------------------------------------------------------------
+// var x=window.prompt('Enter Your Number :')
+
+// for (var i = 1; i<=x ; i++) {   
+//     if (i%2==0) {                                                                eleven
+//         console.log(i);
+//     }                                               
+                                                                          
+// }
+// -----------------------------------------------------------------------------------------------
+// var x=window.prompt('Enter Your Number :');
+// var y=window.prompt('Enter Your Number :');
+// var z=1;
+// for (i  = 1; i <= y; i++) {                                                          
+//     z=z*x
+// }
+//         console.log(z);
+
+// ------------------------------------------------------------------------------------------------
+// var x=+window.prompt('Enter Your Number :');
+// var y=+window.prompt('Enter Your Number :');
+// var z=+window.prompt('Enter Your Number :');
+// var s=+window.prompt('Enter Your Number :');
+// var n=+window.prompt('Enter Your Number :');                                             12
+// console.log('Total marks =',x+y+z+s+n);
+// console.log(' Average Marks =',x+y+z+s+n/5);
+// console.log(' Percentage =',x+y+z+s+n/5);
+// --------------------------------------------------------------------------------------------------
+// var x=window.prompt('Month Number : ');
+// if (x==1) {
+//     console.log( 'Days in Month:', 31 );
+// }else if (x==2) {
+//     console.log( 'Days in Month:', 28 );
+// }else if (x==3) {
+//     console.log( 'Days in Month:', 31 );
+// }else if (x==4) {
+//     console.log( 'Days in Month:', 30 );
+// }else if (x==5) {
+//     console.log( 'Days in Month:', 31 );                                                  13
+// }else if (x==6) {
+//     console.log( 'Days in Month:', 30 );
+// }else if (x==7) {
+//     console.log( 'Days in Month:', 31 );
+// }else if (x==8) {
+//     console.log( 'Days in Month:', 31 );
+// }else if (x==9) {
+//     console.log( 'Days in Month:', 30 );
+// }else if (x==10) {
+//     console.log( 'Days in Month:', 31 );
+// }else if (x==11) {
+//     console.log( 'Days in Month:', 30 );
+// }else if (x==12) {
+//     console.log( 'Days in Month:', 31 );
+// }else{
+//     console.log('false');
+// }
+// ---------------------------------------------------------------------------------------------------
+// var x=+window.prompt('Enter Your grad Physics:');
+// var y=+window.prompt('Enter Your grad Chemistry:');
+// var z=+window.prompt('Enter Your grad Biology:');
+// var e=+window.prompt('Enter Your grad Mathematics :');
+// var n=+window.prompt('Enter Your grad Computer :');
+
+// var percentage = ((x+y+z+e+n)/500)*100
+// console.log('percentage :',percentage);
+// if (percentage>=90) {                                                                  14
+//     console.log(' Grad A ');
+// }else if(percentage>= 80){
+//     console.log(' Grad B ');
+// }else if (percentage >= 70) {
+//     console.log(' Grad C ');
+// }else if (percentage >= 60) {
+//     console.log(' Grad D');
+// }else if (percentage  >= 40) {
+//     console.log(' Grad E ');
+// }else if (percentage< 40) {
+//     console.log(' Grad F');
+// }else{
+//     console.log('false');
+// }
+// -----------------------------------------------------------------------------------------------------
+                                        //    switch case
+
+// var x=+window.prompt('Enter Your number month :');
+// switch (x) {
+//     case 1:
+//         console.log('total number of days in month :',31);
+//         break;
+//         case 2:
+//             console.log('total number of days in month :',28);                                             15
+//             break;
+//             case 3:
+//                 console.log('total number of days in month :',31);
+//                 break;
+//                 case 4:
+//                 console.log('total number of days in month :',30);
+//                 break;
+//                 case 5:
+//                 console.log('total number of days in month :',31);
+//                 break;
+//                 case 6:
+//                 console.log('total number of days in month :',30);
+//                 break;
+//                 case 7:
+//                 console.log('total number of days in month :',31);
+//                 break;
+//                 case 8:
+//                 console.log('total number of days in month :',31);
+//                 break;
+//                 case 9:
+//                 console.log('total number of days in month :',30);
+//                 break;
+//                 case 10:
+//                 console.log('total number of days in month :',31);
+//                 break;
+//                 case 11:
+//                 console.log('total number of days in month :',30);
+//                 break;
+//                 case 12:
+//                 console.log('total number of days in month :',31);
+//                 break;
+//     default:
+//         break;
+// }
+// ----------------------------------------------------------------------------------------------------------
+// var x=window.prompt('Enter Your alphabet :');
+
+// switch (x) {
+//     case 'a':
+//         console.log('vowel ');
+//         break;
+//         case 'o':
+//             console.log('vowel ');
+//             break;
+//             case 'u':
+//             console.log('vowel ');                                                               16
+//             break;
+//             case 'e':
+//             console.log('vowel ');
+//             break;
+//             case 'i':
+//             console.log('vowel ');
+//             break;
+//     default:
+//         console.log('consonant ');
+//         break;
+// }
+// -----------------------------------------------------------------------------------------------------------
+// var x=window.prompt('Enter Your num1 :');
+// var y=window.prompt('Enter Your num2 :');
+// switch (true) {
+//     case (x>y):
+//         console.log('max',x);
+//         break;                                                                                     17
+// case (y>x):
+//     console.log('max',y);
+//     default:     
+//         break;
+// }
+// ----------------------------------------------------------------------------------------------------------
+// var x=window.prompt('Enter Your num1 :');
+// switch (true) {
+//     case (x % 2==0):
+//         console.log('number is even');                                                          18
+//         break;
+//     default:     
+//     console.log('number is odd');
+//         break;
+// }
+// -----------------------------------------------------------------------------------------------------------
+
+// var x=window.prompt('Enter Your num1 :');
+// switch (true) {
+//     case (x > 0):
+//         console.log(' number is positive ');                                                         
+//         break;
+//         case (x < 0):
+//         console.log(' number is negative  ');                                                          19
+//         break;
+//     default:     
+//     console.log('number is zero');
+//         break;
+// }
+// ----------------------------------------------------------------------------------------------------------
+// var operator=window.prompt('Enter operator ( either +, -, * or / ) :');
+// var x=+window.prompt('Enter Your num1 :');
+// var y=+window.prompt('Enter Your num1 :');
+
+// switch (operator) {
+//     case '+':
+//         console.log('num1 + num2 =:',x+y);
+//         break;
+//         case '-':
+//             console.log('num1 - num2 =:',x-y);
+//             break;
+//             case '*':                                                                     20
+//             console.log('num1 * num2 =:',x*y);                     
+//             break;
+//             case '/':
+//             console.log('num1 / num2 =:',x/y);
+//             break;
+//     default:
+//         console.log('Invalid operator');
+//         break;
+// }
+// -------------------------------------------------------------------------------------------------------
+var carton=''
+function add() {
+    carton+= `<div class="col-md-4">
+    <img src="12.jpg" alt="" class="w-100 m-2">
+</div>
+`
+document.getElementById('demo').innerHTML=carton
+}
